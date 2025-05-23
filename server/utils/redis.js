9ef01d27ec54df547829aeb30c7acb8091efe930
shashidhar-chinthaparthi-au6 <58417872+shashidhar-chinthaparthi-au6@users.noreply.github.com>
2025-05-24 04:02:57 +0530

    hbjb

--- server/utils/redis.js
+++ server/utils/redis.js
@@ -2,7 +2,21 @@ const redis = require('redis');
 const config = require('../config/config');
 
 const client = redis.createClient({
-  url: `redis://default:${process.env.REDIS_PASSWORD}@${config[process.env.NODE_ENV || 'development'].redis.host}:${config[process.env.NODE_ENV || 'development'].redis.port}`
+  host: config[process.env.NODE_ENV || 'development'].redis.host,
+  port: config[process.env.NODE_ENV || 'development'].redis.port,
+  password: process.env.REDIS_PASSWORD,
+  retry_strategy: function(options) {
+    if (options.error && options.error.code === 'ECONNREFUSED') {
+      return new Error('The server refused the connection');
+    }
+    if (options.total_retry_time > 1000 * 60 * 60) {
+      return new Error('Retry time exhausted');
+    }
+    if (options.attempt > 10) {
+      return undefined;
+    }
+    return Math.min(options.attempt * 100, 3000);
+  }
 });
 
 client.on('error', (err) => console.error('Redis Client Error', err));
