97fce5efa79ae654c62d5e7011ec63c4263e0e4a
shashidhar-chinthaparthi-au6 <58417872+shashidhar-chinthaparthi-au6@users.noreply.github.com>
2025-05-24 03:59:58 +0530

    fix

--- server/utils/redis.js
+++ server/utils/redis.js
@@ -2,11 +2,13 @@ const redis = require('redis');
 const config = require('../config/config');
 
 const client = redis.createClient({
-  host: config[process.env.NODE_ENV || 'development'].redis.host,
-  port: config[process.env.NODE_ENV || 'development'].redis.port
+  url: `redis://default:${process.env.REDIS_PASSWORD}@${config[process.env.NODE_ENV || 'development'].redis.host}:${config[process.env.NODE_ENV || 'development'].redis.port}`
 });
 
 client.on('error', (err) => console.error('Redis Client Error', err));
 client.on('connect', () => console.log('Redis Client Connected'));
+
+// Connect to Redis
+client.connect().catch(console.error);
 
 module.exports = client; 
